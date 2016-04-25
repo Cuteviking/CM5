4feca38d70e41dc6d5a350643cd7ec8e76dcd906
Norrbrinken <pierrenorrbrink@gmail.com>
2016-04-25 10:48:40 +0200

    start

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CM5</title>
+</head>
+
+<body>
+	<form action="php/search.php" method="get" id="search">
+    	Name or Champ<input type="text">
+    </form>
+</body>
+</html>
